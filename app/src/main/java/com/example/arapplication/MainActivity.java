a487cf7a69411d16dd8decf1cd28751829a8b8dd
Vibhor Mathur <matvibhor24@gmail.com>
2023-04-12 16:26:40 +0530

    initial intents

--- app/src/main/java/com/example/arapplication/MainActivity.java
+++ app/src/main/java/com/example/arapplication/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.arapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button start;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        start = findViewById(R.id.getStartedbtn);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
